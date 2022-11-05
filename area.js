df3a889252be2258526465a2f85d474567775893
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-11-05 18:38:29 +0530

    test cases in area

--- area.js
+++ area.js
@@ -1,4 +1,4 @@
- var input = document.querySelectorAll('.input');
+ var inputs = document.querySelectorAll('.input');
 var button = document.querySelector('#cal-btn');
  var output = document.querySelector('#out-put');
 
@@ -6,15 +6,36 @@ button.addEventListener("click",areaOfTriangle);
 function calcProd(a,b)
 {
  
-    var area = a*b;
-   return (area)/2;
+    var area = (a*b)/2;
+    
+   return area;
 
 }
 
 function areaOfTriangle()
 {
- 
-     var areaOfTri=calcProd(Number(input[0].value),Number(input[1].value));
-     output.innerText = `The area of the triangle is ${areaOfTri} ` ;
+     
+     
+     var areaOfTri = calcProd(Number(inputs[0].value),Number(inputs[1].value));
+     if(areaOfTri > 0)
+     {
+          output.innerText = "The area of the triangle is " +areaOfTri;
+     }
+     else{
+          if(inputs[0].value === ""|| inputs[1].value === "")
+          {
+               output.innerText = "Please fill up everything!";
+          }
+          
+          else(inputs[0].value<=0 || inputs[1].value<=0 )
+          {
+               output.innerText = "Please give positive numbers!";
+          }
+     }
+    
 
 }
+    
+    
+
+
