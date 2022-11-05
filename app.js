62dd04bbcf8630a2efdf777a68916c36bd414d7e
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-11-05 17:55:54 +0530

    testcase in sumof angles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var inputs = document.querySelectorAll('.input-angle');
 var isTriBtn = document.querySelector('#is-triangle-btn');
 var output1 = document.querySelector('#out-put');
 function calculateSum(angle1,angle2,angle3){
+  
  var sumOfAngles = angle1 + angle2 + angle3;
 return sumOfAngles;
 }
@@ -10,11 +11,18 @@ function isTriangle()
  var sumOfAngles= calculateSum(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value));
  if(sumOfAngles === 180)
  {
-    output1.innerText = "Yayy! Angles form a triangle";
+    output1.innerText = "Yayy! Angles form a triangle!";
  }
- else
+ else if(inputs[0].value === ""|| inputs[1].value === "" || inputs[2].value === "")
  {
-    output1.innerText ="The angles don't form a triangle";
+   output1.innerText = "Please fill up everything!";
  }
+ else if(inputs[0].value<=0 || inputs[1].value<=0 || inputs[2].value<=0)
+ {
+   output1.innerText = "Please give positive angles!";
+ }
+ else{
+   output1.innerText ="The angles don't form a triangle.";
+}
 }
 isTriBtn.addEventListener("click",isTriangle)
